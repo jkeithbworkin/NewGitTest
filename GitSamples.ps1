0a115558c555ca2b001eb863f31fc7b417f675aa
J. Keith Bankston <jkeithb@hotmail.com>
2015-12-28 14:45:59 -0800

    Testing the git add feature.

---
+++ GitSamples.ps1
@@ -0,0 +1,23 @@
+﻿<# 
+Keith's Github reminders
+In the PS window, you can do the following:
+#>
+
+# this creates a local copy of the URL you were working with
+cd C:\GitHub
+git clone <url> 
+
+<# 
+Git Status shows the current state of the environment
+try modifying any file in the folder, saving it, and then running git status
+you can also copy a file into the folder, & see what the results are
+#>
+git status
+
+# Git Add will add a new file, but not commit it. 
+# try git add 
+git add .\GitSamples.ps1
+git status
+
+# git commit is required to finish the task
+
