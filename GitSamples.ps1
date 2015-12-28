f1d8e4d4d57b4bd6d059993a97894f5c27650fb8
J. Keith Bankston <jkeithb@hotmail.com>
2015-12-28 15:05:03 -0800

    Adding the new file for the first time

--- GitSamples.ps1
+++ GitSamples.ps1
@@ -14,10 +14,17 @@ you can also copy a file into the folder, & see what the results are
 #>
 git status
 
-# Git Add will add a new file, but not commit it. 
+# Git Add will add a new file or add changes to an exising file, but not commit it. 
 # try git add 
 git add .\GitSamples.ps1
 git status
 
 # git commit is required to finish the task
+git commit .\GitSamples.ps1 -m "Testing the git add feature."
 
+<# 
+At this point, the changes are ready, but only local to this system. 
+To get them posted to the git repo this local repo came from, use the command below
+#>
+
+git push --all --repo=https://github.com/jkeithbworkin/NewGitTest.git --dry-run
